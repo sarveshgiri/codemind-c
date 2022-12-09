2359fc7ad59cefa56f4aa5a3214686d76bad9385
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2022-12-09 10:32:58 +0530

    #include<stdio.h> int main() {     int n,a,b,i;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         if(i%2!=0) {            printf("%d x %d = %d ",a,i,a*i);         }     } }

---
+++ Tables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        if(i%2!=0) {
+           printf("%d x %d = %d
+",a,i,a*i); 
+        }
+    }
+}
