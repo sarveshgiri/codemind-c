047ad160373eab4cd7b092455052dbcf1ed2771c
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2022-12-17 15:40:08 +0530

    #include<stdio.h> int main() {     char c;     scanf("%c",&c);     if(c=='a'||c=='A')     {         printf("VOWEL");     }     else if(c=='e'||c=='E')     {         printf("VOWEL");     }     else if(c=='i'||c=='I')     {         printf("VOWEL");     }     else if(c=='o'||c=='O')     {         printf("VOWEL");     }     else if(c=='u'||c=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c=='a'||c=='A')
+    {
+        printf("VOWEL");
+    }
+    else if(c=='e'||c=='E')
+    {
+        printf("VOWEL");
+    }
+    else if(c=='i'||c=='I')
+    {
+        printf("VOWEL");
+    }
+    else if(c=='o'||c=='O')
+    {
+        printf("VOWEL");
+    }
+    else if(c=='u'||c=='U')
+    {
+        printf("VOWEL");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
