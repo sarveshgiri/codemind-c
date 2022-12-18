31dc323e0b7bf1c49132f02f6b7447743859fea2
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2022-12-18 18:49:33 +0530

    #include<stdio.h> int main() {     int n,q,r,s=0,max=0;     scanf("%d",&n);     q=n;     while(q!=0)     {         r=q%10;         if(r>max)         {             max=r;         }         q=q/10;     }     printf("%d",max); }

---
+++ Largest_Digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,q,r,s=0,max=0;
+    scanf("%d",&n);
+    q=n;
+    while(q!=0)
+    {
+        r=q%10;
+        if(r>max)
+        {
+            max=r;
+        }
+        q=q/10;
+    }
+    printf("%d",max);
+}
