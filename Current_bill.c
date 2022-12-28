3f1a2f64157a4a7c58a6b4a775f66e6116fa1699
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2022-12-28 23:04:52 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     float uc,b,t;     if(n<=199)     {         uc=1.20;     }     else if(n>=200&&n<400)     {         uc=1.50;     }     else if(n>=400&&n<600)     {         uc=1.80;     }     if(n>=600)     {         uc=2.00;     }     b=uc*n;     if(b>=400)     {         t=b+b*.15;     }     else     {         t=b+100;     }     printf("%.2f",t); }

--- Current_bill.c
+++ Current_bill.c
@@ -3,32 +3,31 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    float b,c,s;
+    float uc,b,t;
     if(n<=199)
     {
-        c=1.20;
+        uc=1.20;
     }
-     else if(n>=200&&n<400)
+    else if(n>=200&&n<400)
     {
-        c=1.50;
+        uc=1.50;
     }
     else if(n>=400&&n<600)
     {
-        c=1.80;
+        uc=1.80;
     }
-    else if(n>=600)
+    if(n>=600)
     {
-        c=2.00;
+        uc=2.00;
     }
-    b=n*c;
-    if(b<400)
+    b=uc*n;
+    if(b>=400)
     {
-        s=100;
+        t=b+b*.15;
     }
     else
     {
-        s=b*0.15;
+        t=b+100;
     }
-    float tb=b+s;
-    printf("%.2f",tb);
+    printf("%.2f",t);
 }
