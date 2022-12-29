6d0aa3e108a2fe39ec5055006fdf7a9b562c6ae6
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2022-12-29 17:10:18 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int a;         scanf("%d",&a);         if(a<3)         {             printf("LIGHT ");         }         else if(a>=3&&a<7)         {             printf("MODERATE ");         }         else if(a>=7)         {             printf("HEAVY ");         }     } }

---
+++ Rain_gauge.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a;
+        scanf("%d",&a);
+        if(a<3)
+        {
+            printf("LIGHT
+");
+        }
+        else if(a>=3&&a<7)
+        {
+            printf("MODERATE
+");
+        }
+        else if(a>=7)
+        {
+            printf("HEAVY
+");
+        }
+    }
+}
