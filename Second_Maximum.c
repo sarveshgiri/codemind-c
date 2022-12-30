c998130797e7490e0ee637121f9af445ecc235df
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2022-12-30 18:46:51 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int a,b,c;         scanf("%d%d%d",&a,&b,&c);         if(a>b&&a<c||a>c&&a<b) printf("%d ",a);         else if(b>a&&b<c||b>c&&b<a) printf("%d ",b);         else printf("%d ",c);     } }

---
+++ Second_Maximum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        if(a>b&&a<c||a>c&&a<b) printf("%d
+",a);
+        else if(b>a&&b<c||b>c&&b<a) printf("%d
+",b);
+        else printf("%d
+",c);
+    }
+}
