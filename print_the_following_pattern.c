e2b2ec2af69b256c168f70ba9aac5f9a7e4c6ce5
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2023-01-04 22:46:06 +0530

    #include<stdio.h> int main() {     int n,j;     char i;     scanf("%d",&n);     for(i=n+65-1;i>=65;i--)     {         for(j=n;j>=1;j--)         {             printf("%c ",i);         }         printf(" ");         n--;     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,j;
+    char i;
     scanf("%d",&n);
-    for(i=65;i<=65+n-1;i++)
+    for(i=n+65-1;i>=65;i--)
     {
-        for(j=1;j<=n;j++)
+        for(j=n;j>=1;j--)
         {
             printf("%c ",i);
         }
         printf("
 ");
+        n--;
     }
 }
