ddc6f4cf376802055efb395e9ada97d5bb02ca96
Sarvesh Giri <119730478+sarveshgiri@users.noreply.github.com>
2023-01-05 23:15:00 +0530

    #include<stdio.h> int main() {     int n,i,c,j;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(x[i]==x[j]) c++;             }         }         if(c==0) printf("%d",x[i]);     }
    }

---
+++ Single_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c,j;
+    scanf("%d",&n);
+    int x[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(x[i]==x[j]) c++;
+            }
+        }
+        if(c==0) printf("%d",x[i]);
+    }
+
+}
